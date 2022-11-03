81df42324c3fd120dd9b2b706ed46bd90dd8b955
nipunakusal98 <nipuna.suraweera98@gmail.com>
2022-11-03 14:53:03 +0530

    added search by id api and update employee api

--- ems/src/main/java/com/SpringBoot/ems/controller/EmployeeController.java
+++ ems/src/main/java/com/SpringBoot/ems/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.SpringBoot.ems.controller;
 
+import com.SpringBoot.ems.exception.ResourceNotFoundException;
 import com.SpringBoot.ems.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -29,5 +31,28 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+    //get employee by id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable int id){
+        Employee employee =employeeRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("Employee does not exist: "+id));
+        return ResponseEntity.ok(employee);
+
+    }
+
+    //update Employee Rest API
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable int id, @RequestBody Employee employeeDetails){
+        Employee employee =employeeRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("Employee does not exist: "+id));
+        employee.setFirst_name(employeeDetails.getFirst_name());
+        employee.setLast_name(employeeDetails.getLast_lame());
+        employee.setDate_of_birth(employeeDetails.getDate_of_birth());
+        employee.setSalary(employeeDetails.getSalary());
+
+        Employee updatedEmployee= employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+
     }
 }
